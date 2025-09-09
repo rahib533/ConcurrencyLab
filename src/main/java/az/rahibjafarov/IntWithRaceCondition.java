08ea97de03df0d367e5109a902af55bcbdccbb3a
Rahib Jafarov <rahib.cefer.98@gmail.com>
2025-09-10 03:44:52 +0400

    IntWithRaceCondition

---
+++ src/main/java/az/rahibjafarov/IntWithRaceCondition.java
@@ -0,0 +1,20 @@
+package az.rahibjafarov;
+
+public class IntWithRaceCondition {
+    static int counter = 0;
+
+    public static void run() throws InterruptedException {
+        Runnable r = () -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                counter++;
+            }
+        };
+
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        t1.start(); t2.start();
+        t1.join(); t2.join();
+
+        System.out.println("Counter: " + counter);
+    }
+}
---
+++ src/main/java/az/rahibjafarov/Main.java
@@ -0,0 +1,8 @@
+package az.rahibjafarov;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        IntWithRaceCondition.run();
+    }
+}
